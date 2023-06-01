378d0d816597a34efb4c2da87f7f07b1cd23bae0
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2023-06-01 21:56:46 +0200

    refactor commit

--- src/Yatzy.ts
+++ src/Yatzy.ts
@@ -1,187 +1,184 @@
+
 export default class Yatzy {
 
-  static chance(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var total = 0;
-    total += d1;
-    total += d2;
-    total += d3;
-    total += d4;
-    total += d5;
-    return total;
-  }
-
-  static yatzy(...args: number[]): number {
-    var counts = [0, 0, 0, 0, 0, 0, 0, 0];
-    for (var i = 0; i != args.length; ++i) {
-      var die = args[i];
-      counts[die - 1]++;
-    }
-    for (i = 0; i != 6; i++) if (counts[i] == 5) return 50;
-    return 0;
-  }
-
   static ones(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var sum = 0;
-    if (d1 == 1) sum++;
-    if (d2 == 1) sum++;
-    if (d3 == 1) sum++;
-    if (d4 == 1) sum++;
-    if (d5 == 1) sum++;
-
-    return sum;
+    return this.sumAllWhereValue(is(1), [d1, d2, d3, d4, d5]);
   }
 
   static twos(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var sum = 0;
-    if (d1 == 2) sum += 2;
-    if (d2 == 2) sum += 2;
-    if (d3 == 2) sum += 2;
-    if (d4 == 2) sum += 2;
-    if (d5 == 2) sum += 2;
-    return sum;
+    return this.sumAllWhereValue(is(2), [d1, d2, d3, d4, d5]);
   }
 
   static threes(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var s;
-    s = 0;
-    if (d1 == 3) s += 3;
-    if (d2 == 3) s += 3;
-    if (d3 == 3) s += 3;
-    if (d4 == 3) s += 3;
-    if (d5 == 3) s += 3;
-    return s;
-  }
-
-  static onePair(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    counts[d1 - 1]++;
-    counts[d2 - 1]++;
-    counts[d3 - 1]++;
-    counts[d4 - 1]++;
-    counts[d5 - 1]++;
-    var at;
-    for (at = 0; at != 6; at++) if (counts[6 - at - 1] >= 2) return (6 - at) * 2;
-    return 0;
-  }
-
-  static twoPairs(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    counts[d1 - 1]++;
-    counts[d2 - 1]++;
-    counts[d3 - 1]++;
-    counts[d4 - 1]++;
-    counts[d5 - 1]++;
-    var n = 0;
-    var score = 0;
-    for (let i = 0; i < 6; i += 1)
-      if (counts[6 - i - 1] >= 2) {
-        n++;
-        score += 6 - i;
-      }
-    if (n == 2) return score * 2;
-    else return 0;
-  }
-
-  static fourOfAKind(_1: number, _2: number, d3: number, d4: number, d5: number): number {
-    var tallies;
-    tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-    tallies[_1 - 1]++;
-    tallies[_2 - 1]++;
-    tallies[d3 - 1]++;
-    tallies[d4 - 1]++;
-    tallies[d5 - 1]++;
-    for (let i = 0; i < 6; i++) if (tallies[i] >= 4) return (i + 1) * 4;
-    return 0;
-  }
-
-  static threeOfAKind(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var t;
-    t = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    t[d1 - 1]++;
-    t[d2 - 1]++;
-    t[d3 - 1]++;
-    t[d4 - 1]++;
-    t[d5 - 1]++;
-    for (let i = 0; i < 6; i++) if (t[i] >= 3) return (i + 1) * 3;
-    return 0;
-  }
-
-  static smallStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var tallies;
-    tallies = [0, 0, 0, 0, 0, 0, 0];
-    tallies[d1 - 1] += 1;
-    tallies[d2 - 1] += 1;
-    tallies[d3 - 1] += 1;
-    tallies[d4 - 1] += 1;
-    tallies[d5 - 1] += 1;
-    if (tallies[0] == 1 && tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1) return 15;
-    return 0;
-  }
-
-  static largeStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var tallies;
-    tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-    tallies[d1 - 1] += 1;
-    tallies[d2 - 1] += 1;
-    tallies[d3 - 1] += 1;
-    tallies[d4 - 1] += 1;
-    tallies[d5 - 1] += 1;
-    if (tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1 && tallies[5] == 1) return 20;
-    return 0;
-  }
-
-  static fullHouse(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var tallies;
-    var _2 = false;
-    var i;
-    var _2_at = 0;
-    var _3 = false;
-    var _3_at = 0;
-
-    tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-    tallies[d1 - 1] += 1;
-    tallies[d2 - 1] += 1;
-    tallies[d3 - 1] += 1;
-    tallies[d4 - 1] += 1;
-    tallies[d5 - 1] += 1;
-
-    for (i = 0; i != 6; i += 1)
-      if (tallies[i] == 2) {
-        _2 = true;
-        _2_at = i + 1;
-      }
-
-    for (i = 0; i != 6; i += 1)
-      if (tallies[i] == 3) {
-        _3 = true;
-        _3_at = i + 1;
-      }
-
-    if (_2 && _3) return _2_at * 2 + _3_at * 3;
-    else return 0;
+    return this.sumAllWhereValue(is(3), [d1, d2, d3, d4, d5]);
   }
 
   static fours(...p: number[]): number {
-    var sum;
-    sum = 0;
-    for (let at = 0; at != 5; at++) {
-      if (p[at] == 4) {
-        sum += 4;
-      }
-    }
-    return sum;
+    return this.sumAllWhereValue(is(4), p);
   }
 
   static fives(...dice: number[]): number {
-    let s = 0;
-    var i;
-    for (i = 0; i < dice.length; i++) if (dice[i] == 5) s = s + 5;
-    return s;
+    return this.sumAllWhereValue(is(5), dice);
   }
 
   static sixes(...dice: number[]): number {
-    let sum = 0;
-    for (var at = 0; at < dice.length; at++) if (dice[at] == 6) sum = sum + 6;
-    return sum;
+    return this.sumAllWhereValue(is(6), dice);
   }
+
+  static chance(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    return this.sumAllWhereValue(isAny(), [d1, d2, d3, d4, d5]);
+  }
+
+  private static sumAllWhereValue(diceFilter: (dice: number) => boolean, dices: number[]): number {
+    return dices.filter(diceFilter).reduce(sum, 0);
+  }
+
+
+
+  static onePair(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    return this.moreOfaKind(2, [d1, d2, d3, d4, d5]);
+  }
+
+  static twoPairs(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    return this.moreOfaKindMultipleGroups(2, [d1, d2, d3, d4, d5], 2);
+  }
+
+  static threeOfAKind(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    return this.moreOfaKind(3, [d1, d2, d3, d4, d5]);
+  }
+
+  static fourOfAKind(_1: number, _2: number, d3: number, d4: number, d5: number): number {
+    return this.moreOfaKind(4, [_1, _2, d3, d4, d5]);
+  }
+
+  static yatzy(...args: number[]): number {
+    return this.hasMoreOfaKind(5, args) ? 50 : 0;
+  }
+
+  static moreOfaKind(count: number, roll: number[]): number {
+    return this.moreOfaKindMultipleGroups(count, roll, 1);
+  }
+
+  private static moreOfaKindMultipleGroups(count: number, dices: number[], nrOfGroups: number): number {
+    const roll: Roll = dices.reduce(byFrequency, Roll.new());
+    return roll.sortedByValue()
+      .filter(dice => dice.count >= count)
+      .map(dice => dice.value * count)
+      .slice(0, nrOfGroups)
+      .reduce(sumIfLength(is(nrOfGroups)), 0);
+  }
+
+  private static hasMoreOfaKind(nrOfElements: number, dices: number[]): boolean {
+    return this.moreOfaKind(nrOfElements, dices) != 0;
+  }
+
+  static smallStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    return this.straight([d1, d2, d3, d4, d5], first(5), 15);
+  }
+
+  static largeStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    return this.straight([d1, d2, d3, d4, d5], last(5), 20);
+  }
+
+  private static straight(dices: number[], takeFromList: (arr: any[]) => any[], score: number): number {
+    const roll: Roll = dices.reduce(byFrequency, Roll.new());
+    const doesMatch = takeFromList(roll.dices())
+      .map(dice => dice.count)
+      .filter(is(1))
+      .length == 5;
+    return doesMatch ? score : 0;
+  }
+
+  static fullHouse(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    const roll: Roll = [d1, d2, d3, d4, d5].reduce(byFrequency, Roll.new());
+    const doesMatch = roll.dices()
+      .map(dice => dice.count)
+      .filter(c => c == 2 || c == 3)
+      .reduce(sum, 0) == 5;
+    return doesMatch ? roll.dices().map(d => d.count * d.value).reduce(sum, 0) : 0;
+  }
+}
+
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+function sumIfLength(lengthPredicate: any): any {
+  return (acc: number, val: number, index: number, arr: number[]) => {
+    if (lengthPredicate(arr.length)) {
+      return acc + val;
+    }
+    return 0;
+  }
+
+}
+
+function is(expected: number) {
+  return (x: number) => x == expected;
+}
+function isAny() {
+  return (__: number) => true;
+}
+
+function byFrequency(roll: Roll, curr: number): Roll {
+  return roll.add(curr);
+}
+
+
+class Roll {
+  private readonly _dices: Dice[];
+
+  public static new(): Roll {
+    return new Roll(
+      [1, 2, 3, 4, 5, 6].map(dice => new Dice(dice, 0))
+    );
+  }
+
+  constructor(d: Dice[]) {
+    this._dices = d;
+  }
+
+  public add(newDice: number): Roll {
+    const newDices = this._dices
+      .map(dice => dice.value == newDice ? dice.increased() : dice);
+    return new Roll(newDices);
+  }
+
+  public sortedByValue(): Dice[] {
+    return this._dices
+      .sort((d1, d2) => d1.hasBiggerValueThan(d2) ? -1 : 1)
+  }
+
+  public dices(): Dice[] {
+    return [...this._dices];
+  }
+}
+
+class Dice {
+  public readonly value: number;
+  public readonly count: number;
+
+  constructor(v: number, c: number) {
+    this.value = v;
+    this.count = c;
+  }
+
+  public increased(): Dice {
+    return new Dice(
+      this.value,
+      this.count + 1
+    )
+  }
+
+  public hasBiggerValueThan(other: Dice): boolean {
+    return this.value > other.value;
+  }
+}
+
+function last(nrOfElements: number): (arr: any[]) => any[] {
+  return (arr: any[]) => [...arr].slice(arr.length - nrOfElements, arr.length);
+}
+
+function first(nrOfElements: number): (arr: any[]) => any[] {
+  return (arr: any[]) => [...arr].slice(0, nrOfElements);
 }
